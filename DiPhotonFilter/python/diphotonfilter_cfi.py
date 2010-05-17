338aa8afad5dfe4cb6c59d7765c5a0786d627f53
chenders <>
2010-05-17 13:25:53 +0000

    Simple filter on diphoton events, with different pt cuts for leading and second photon

---
+++ DiPhotonFilter/python/diphotonfilter_cfi.py
@@ -0,0 +1,9 @@
+import FWCore.ParameterSet.Config as cms
+
+diphotonFilter = cms.EDFilter('DiPhotonFilter',
+                              photonCollection = cms.untracked.InputTag("photons"),
+                              ptMin_photon1 = cms.untracked.double(10),
+                              ptMin_photon2 = cms.untracked.double(10)
+
+
+)
