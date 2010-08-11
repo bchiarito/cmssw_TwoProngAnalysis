85071dd75e86fc1571bab082adc142de9dba52ee
chenders <>
2010-08-11 09:49:52 +0000

    revised HLT bits based on first list from Toyoko. Changed from saving bit as bool to int in struct/tree, so that -1 can be used to indicate when the path was not even present in the current event. Added a FillHLTInfo function so that all HLT-specific stuff now localised in this file, and any analyser can just include it

--- CommonClasses/interface/TriggerInfo.h
+++ CommonClasses/interface/TriggerInfo.h
@@ -13,6 +13,9 @@
 
 #include <string>
 
+#include "DataFormats/Common/interface/TriggerResults.h"
+#include "FWCore/Framework/interface/TriggerNames.h"
+
 namespace ExoDiPhotons
 {
 
@@ -42,45 +45,137 @@ namespace ExoDiPhotons
 
 
 
-  // HLT info - MinBias trigger?  Definitely photon triggers!
-  // this will need to be kept up to date since the HLT triggers evolve
+  // HLT info 
+  // do far going with: MinBias, single and double photon, L1_EGs,
+  // but not electron or photon+X
+  // this list will need to be kept up to date since the HLT triggers evolve
   // eg with spike cleaning, prescales, etc
-  // store prescale value too?
+  // note that we use an int,, where 0=fail and 1=pass obviously, 
+  // and -1=not present in menu for this event
+  //   should we  store prescale value too?
   struct hltTrigInfo_t{
-    bool HLT_MinBiasBSC;
-    bool HLT_MinBiasBSC_NoBPTX;
-    bool HLT_MinBiasBSC_OR;
-    bool HLT_L1_BscMinBiasOR_BptxPlusORMinus;
-    bool HLT_Jet15U;
-    bool HLT_Jet30U;
-    bool HLT_Jet50U;
-    bool HLT_L1SingleEG2;
-    bool HLT_L1SingleEG2_NoBPTX;
-    bool HLT_L1SingleEG5;
-    bool HLT_L1SingleEG5_NoBPTX;
-    bool HLT_L1SingleEG8;
-    bool HLT_L1SingleEG20_NoBPTX;
-    bool HLT_L1DoubleEG5;
-    bool HLT_EgammaSuperClusterOnly_L1R;
-    bool HLT_Photon10_L1R;
-    bool HLT_Photon15_L1R;
-    bool HLT_Photon15_TrackIso_L1R;
-    bool HLT_Photon15_LooseEcalIso_L1R;
-    bool HLT_Photon20_L1R;
-    bool HLT_Photon30_L1R_8E29;
-    bool HLT_DoublePhoton5_L1R;
-    bool HLT_DoublePhoton10_L1R;
-
+    int HLT_MinBiasBSC;
+    int HLT_MinBiasBSC_NoBPTX;
+    int HLT_MinBiasBSC_OR;
+    int HLT_L1_BscMinBiasOR_BptxPlusORMinus;
+    int HLT_L1SingleEG2;
+    int HLT_L1SingleEG5;
+    int HLT_L1SingleEG8;
+    int HLT_L1DoubleEG5;
+    int HLT_Photon10_Cleaned_L1R;
+    int HLT_Photon15_Cleaned_L1R;
+    int HLT_Photon15_L1R;
+    int HLT_Photon15_LooseEcalIso_L1R;
+    int HLT_Photon15_TrackIso_L1R;
+    int HLT_Photon20_Cleaned_L1R;
+    int HLT_Photon20_L1R;
+    int HLT_Photon30_Cleaned_L1R;
+    int HLT_Photon30_L1R_8E29;
+    int HLT_Photon50_Cleaned_L1R;
+    int HLT_Photon50_L1R;
+    int HLT_DoublePhoton5_L1R;
+    int HLT_DoublePhoton10_L1R;
+    int HLT_DoublePhoton15_L1R;
+    int HLT_DoublePhoton20_L1R;
   };
 
   // string for defining a tree branch for this struct
-  std::string hltTrigBranchDefString(":HLT_MinBiasBSC/O:HLT_MinBiasBSC_NoBPTX:HLT_MinBiasBSC_OR:HLT_L1_BscMinBiasOR_BptxPlusORMinus:HLT_Jet15U/O:HLT_Jet30U:HLT_Jet50U:HLT_L1SingleEG2:HLT_L1SingleEG2_NoBPTX:HLT_L1SingleEG5:HLT_L1SingleEG5_NoBPTX:HLT_L1SingleEG8:HLT_L1SingleEG20_NoBPTX:HLT_L1DoubleEG5:HLT_EgammaSuperClusterOnly_L1R:HLT_Photon10_L1R:HLT_Photon15_L1R:HLT_Photon15_TrackIso_L1R:HLT_Photon15_LooseEcalIso_L1R:HLT_Photon20_L1R:HLT_Photon30_L1R_8E29:HLT_DoublePhoton5_L1R:HLT_DoublePhoton10_L1R");
+  std::string hltTrigBranchDefString("HLT_MinBiasBSC/I:HLT_MinBiasBSC_NoBPTX:HLT_MinBiasBSC_OR:HLT_L1_BscMinBiasOR_BptxPlusORMinus:HLT_L1SingleEG2:HLT_L1SingleEG5:HLT_L1SingleEG8:HLT_L1DoubleEG5:HLT_Photon10_Cleaned_L1R:HLT_Photon15_Cleaned_L1R:HLT_Photon15_L1R:HLT_Photon15_LooseEcalIso_L1R:HLT_Photon15_TrackIso_L1R:HLT_Photon20_Cleaned_L1R:HLT_Photon20_L1R:HLT_Photon30_Cleaned_L1R:HLT_Photon30_L1R_8E29:HLT_Photon50_Cleaned_L1R:HLT_Photon50_L1R:HLT_DoublePhoton5_L1R:HLT_DoublePhoton10_L1R:HLT_DoublePhoton15_L1R:HLT_DoublePhoton20_L1R");
   
   // need also to have an Initialise() function?
+  // No, we can do this inside the Fill function ...
 
-  // Fill() function for HLT info to appear here ....
+  // Fill() function for HLT info
+  // needs both a TriggerResults and TriggerNames object
+
+  void FillHLTInfo(hltTrigInfo_t &hltInfo, const TriggerResults *hltResults, const TriggerNames &hltNames) {
+
+    // first set all the triggers to -1
+    // then if the path is not even present in the menu for this event
+    // it will remain at -1, while regular pass/fail are 1/0
+
+    hltInfo.HLT_MinBiasBSC = -1;
+    hltInfo.HLT_MinBiasBSC_NoBPTX = -1;
+    hltInfo.HLT_MinBiasBSC_OR = -1;
+    hltInfo.HLT_L1_BscMinBiasOR_BptxPlusORMinus = -1;
+    hltInfo.HLT_L1SingleEG2 = -1;
+    hltInfo.HLT_L1SingleEG5 = -1;
+    hltInfo.HLT_L1SingleEG8 = -1;
+    hltInfo.HLT_L1DoubleEG5 = -1;
+    hltInfo.HLT_Photon10_Cleaned_L1R = -1;
+    hltInfo.HLT_Photon15_Cleaned_L1R = -1;
+    hltInfo.HLT_Photon15_L1R = -1;
+    hltInfo.HLT_Photon15_LooseEcalIso_L1R = -1;
+    hltInfo.HLT_Photon15_TrackIso_L1R = -1;
+    hltInfo.HLT_Photon20_Cleaned_L1R = -1;
+    hltInfo.HLT_Photon20_L1R = -1;
+    hltInfo.HLT_Photon30_Cleaned_L1R = -1;
+    hltInfo.HLT_Photon30_L1R_8E29 = -1;
+    hltInfo.HLT_Photon50_Cleaned_L1R = -1;
+    hltInfo.HLT_Photon50_L1R = -1;
+    hltInfo.HLT_DoublePhoton5_L1R = -1;
+    hltInfo.HLT_DoublePhoton10_L1R = -1;
+    hltInfo.HLT_DoublePhoton15_L1R = -1;
+    hltInfo.HLT_DoublePhoton20_L1R = -1;
 
 
+
+    // we'll just loop over all triggers in the current event
+    // and check one-by-one the result for the triggers we are interested in
+
+    for(int itrig=0;itrig<hltResults->size();itrig++) {
+
+      if(hltNames.triggerName(itrig)=="HLT_MinBiasBSC")
+	hltInfo.HLT_MinBiasBSC = (int) hltResults->accept(itrig);
+      else if(hltNames.triggerName(itrig)=="HLT_MinBiasBSC_NoBPTX")
+	hltInfo.HLT_MinBiasBSC_NoBPTX = (int) hltResults->accept(itrig);
+      else if(hltNames.triggerName(itrig)=="HLT_MinBiasBSC_OR")
+	hltInfo.HLT_MinBiasBSC_OR = (int) hltResults->accept(itrig);
+      else if(hltNames.triggerName(itrig)=="HLT_L1_BscMinBiasOR_BptxPlusORMinus")
+	hltInfo.HLT_L1_BscMinBiasOR_BptxPlusORMinus = (int) hltResults->accept(itrig);
+      else if(hltNames.triggerName(itrig)=="HLT_L1SingleEG2")
+	hltInfo.HLT_L1SingleEG2 = (int) hltResults->accept(itrig);
+      else if(hltNames.triggerName(itrig)=="HLT_L1SingleEG5")
+	hltInfo.HLT_L1SingleEG5 = (int) hltResults->accept(itrig);
+      else if(hltNames.triggerName(itrig)=="HLT_L1SingleEG8")
+	hltInfo.HLT_L1SingleEG8 = (int) hltResults->accept(itrig);
+      else if(hltNames.triggerName(itrig)=="HLT_L1DoubleEG5")
+	hltInfo.HLT_L1DoubleEG5 = (int) hltResults->accept(itrig);
+      else if(hltNames.triggerName(itrig)=="HLT_Photon10_Cleaned_L1R")
+	hltInfo.HLT_Photon10_Cleaned_L1R = (int) hltResults->accept(itrig);
+      else if(hltNames.triggerName(itrig)=="HLT_Photon15_Cleaned_L1R")
+	hltInfo.HLT_Photon15_Cleaned_L1R = (int) hltResults->accept(itrig);
+      else if(hltNames.triggerName(itrig)=="HLT_Photon15_L1R")
+	hltInfo.HLT_Photon15_L1R = (int) hltResults->accept(itrig);
+      else if(hltNames.triggerName(itrig)=="HLT_Photon15_LooseEcalIso_L1R")
+	hltInfo.HLT_Photon15_LooseEcalIso_L1R = (int) hltResults->accept(itrig);
+      else if(hltNames.triggerName(itrig)=="HLT_Photon15_TrackIso_L1R")
+	hltInfo.HLT_Photon15_TrackIso_L1R = (int) hltResults->accept(itrig);
+      else if(hltNames.triggerName(itrig)=="HLT_Photon20_Cleaned_L1R")
+	hltInfo.HLT_Photon20_Cleaned_L1R = (int) hltResults->accept(itrig);
+      else if(hltNames.triggerName(itrig)=="HLT_Photon20_L1R")
+	hltInfo.HLT_Photon20_L1R = (int) hltResults->accept(itrig);
+      else if(hltNames.triggerName(itrig)=="HLT_Photon30_Cleaned_L1R")
+	hltInfo.HLT_Photon30_Cleaned_L1R = (int) hltResults->accept(itrig);
+      else if(hltNames.triggerName(itrig)=="HLT_Photon30_L1R_8E29")
+	hltInfo.HLT_Photon30_L1R_8E29 = (int) hltResults->accept(itrig);
+      else if(hltNames.triggerName(itrig)=="HLT_Photon50_Cleaned_L1R")
+	hltInfo.HLT_Photon50_Cleaned_L1R = (int) hltResults->accept(itrig);
+      else if(hltNames.triggerName(itrig)=="HLT_Photon50_L1R")
+	hltInfo.HLT_Photon50_L1R = (int) hltResults->accept(itrig);
+      else if(hltNames.triggerName(itrig)=="HLT_DoublePhoton5_L1R")
+	hltInfo.HLT_DoublePhoton5_L1R = (int) hltResults->accept(itrig);
+      else if(hltNames.triggerName(itrig)=="HLT_DoublePhoton10_L1R")
+	hltInfo.HLT_DoublePhoton10_L1R = (int) hltResults->accept(itrig);
+      else if(hltNames.triggerName(itrig)=="HLT_DoublePhoton15_L1R")
+	hltInfo.HLT_DoublePhoton15_L1R = (int) hltResults->accept(itrig);
+      else if(hltNames.triggerName(itrig)=="HLT_DoublePhoton20_L1R")
+	hltInfo.HLT_DoublePhoton20_L1R = (int) hltResults->accept(itrig);
+
+    }
+
+
+  }
 
 
 }  //end of namespace
