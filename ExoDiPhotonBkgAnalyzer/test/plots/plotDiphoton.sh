48b2779eab4eb904e22012398c5b5b3e6342cf79
torimoto <>
2010-08-30 12:26:46 +0000

    added option for data

--- ExoDiPhotonBkgAnalyzer/test/plots/plotDiphoton.sh
+++ ExoDiPhotonBkgAnalyzer/test/plots/plotDiphoton.sh
@@ -1,18 +1,30 @@
 #!/bin/bash                                                                                                
+
 SAMPLE=$1;
+DATA="data";
 
 eval `scramv1 runtime -sh`;
 
+if [ ${DATA} == "data" ]; then
+  ISDATA="kTRUE";
+  TITLE="Diphoton: ${SAMPLE}"
+else
+  ISDATA="kFALSE";
+  TITLE="Diphoton: ${SAMPLE} Background MC"
+fi
+
+echo $ISDATA;
+
 root -b <<!
-.x make_diphoton_plots.C("${SAMPLE}");
-.q
+ .x make_diphoton_plots.C("${SAMPLE}",kTRUE,${ISDATA});
+ .q
 !
 
-rfcp histograms_${SAMPLE}.root  /castor/cern.ch/user/t/torimoto/physics/diphoton/ntuples/mc/${SAMPLE};
+rfcp histograms_${SAMPLE}.root  /castor/cern.ch/user/t/torimoto/physics/diphoton/ntuples/${DATA}/${SAMPLE};
 
 # now make webpge
 
-WEBDIR=/afs/cern.ch/user/t/torimoto/www/public_html/physics/diphoton/mc/$1;
+WEBDIR=/afs/cern.ch/user/t/torimoto/www/public_html/physics/diphoton/${DATA}/$1;
 mkdir $WEBDIR;
 mv *$1*.png $WEBDIR;
 
@@ -20,61 +32,61 @@ cat > ${WEBDIR}/index.html <<EOF
 
 <HTML>
 
-<HEAD><TITLE>Diphoton: ${SAMPLE} Background MC</TITLE></HEAD>
+<HEAD><TITLE>${TITLE}</TITLE></HEAD>
 
-<h1><FONT color="Blue">Diphoton: ${SAMPLE} Background MC</FONT></h1>
+<h1><FONT color="Blue">${TITLE}</FONT></h1>
  
 <BODY link="Red">
 <FONT color="Red"> 
 
 <h2> Trigger Info </h2>
-<A HREF=http://torimoto.web.cern.ch/torimoto/physics/diphoton/mc/${SAMPLE}/trigger_${SAMPLE}.png>
-<img height="600" src="http://torimoto.web.cern.ch/torimoto/physics/diphoton/mc/${SAMPLE}/trigger_${SAMPLE}.png">
+<A HREF=http://torimoto.web.cern.ch/torimoto/physics/diphoton/${DATA}/${SAMPLE}/trigger_${SAMPLE}.png>
+<img height="600" src="http://torimoto.web.cern.ch/torimoto/physics/diphoton/${DATA}/${SAMPLE}/trigger_${SAMPLE}.png">
 </A>
 
 <h2> Photon1 Kinematics </h2>
-<A HREF=http://torimoto.web.cern.ch/torimoto/physics/diphoton/mc/${SAMPLE}/kinematics_photon1_${SAMPLE}.png>
-<img height="600" src="http://torimoto.web.cern.ch/torimoto/physics/diphoton/mc/${SAMPLE}/kinematics_photon1_${SAMPLE}.png">
+<A HREF=http://torimoto.web.cern.ch/torimoto/physics/diphoton/${DATA}/${SAMPLE}/kinematics_photon1_${SAMPLE}.png>
+<img height="600" src="http://torimoto.web.cern.ch/torimoto/physics/diphoton/${DATA}/${SAMPLE}/kinematics_photon1_${SAMPLE}.png">
 </A>
 
 <h2> Photon1 HCAL & ECAL Isolation </h2>
-<A HREF=http://torimoto.web.cern.ch/torimoto/physics/diphoton/mc/${SAMPLE}/caloIso_photon1_${SAMPLE}.png>
-<img height="600" src="http://torimoto.web.cern.ch/torimoto/physics/diphoton/mc/${SAMPLE}/caloIso_photon1_${SAMPLE}.png">
+<A HREF=http://torimoto.web.cern.ch/torimoto/physics/diphoton/${DATA}/${SAMPLE}/caloIso_photon1_${SAMPLE}.png>
+<img height="600" src="http://torimoto.web.cern.ch/torimoto/physics/diphoton/${DATA}/${SAMPLE}/caloIso_photon1_${SAMPLE}.png">
 </A>
 
 <h2> Photon1 Track Isolation</h2>
-<A HREF=http://torimoto.web.cern.ch/torimoto/physics/diphoton/mc/${SAMPLE}/trackIso_photon1_${SAMPLE}.png>
-<img height="600" src="http://torimoto.web.cern.ch/torimoto/physics/diphoton/mc/${SAMPLE}/trackIso_photon1_${SAMPLE}.png">
+<A HREF=http://torimoto.web.cern.ch/torimoto/physics/diphoton/${DATA}/${SAMPLE}/trackIso_photon1_${SAMPLE}.png>
+<img height="600" src="http://torimoto.web.cern.ch/torimoto/physics/diphoton/${DATA}/${SAMPLE}/trackIso_photon1_${SAMPLE}.png">
 </A>
 
 <h2> Photon1 Spike Rejection</h2>
-<A HREF=http://torimoto.web.cern.ch/torimoto/physics/diphoton/mc/${SAMPLE}/spike_photon1_${SAMPLE}.png>
-<img height="600" src="http://torimoto.web.cern.ch/torimoto/physics/diphoton/mc/${SAMPLE}/spike_photon1_${SAMPLE}.png">
+<A HREF=http://torimoto.web.cern.ch/torimoto/physics/diphoton/${DATA}/${SAMPLE}/spike_photon1_${SAMPLE}.png>
+<img height="600" src="http://torimoto.web.cern.ch/torimoto/physics/diphoton/${DATA}/${SAMPLE}/spike_photon1_${SAMPLE}.png">
 </A>
 
 <h2> Photon2 Kinematics </h2>
-<A HREF=http://torimoto.web.cern.ch/torimoto/physics/diphoton/mc/${SAMPLE}/kinematics_photon2_${SAMPLE}.png>
-<img height="600" src="http://torimoto.web.cern.ch/torimoto/physics/diphoton/mc/${SAMPLE}/kinematics_photon2_${SAMPLE}.png">
+<A HREF=http://torimoto.web.cern.ch/torimoto/physics/diphoton/${DATA}/${SAMPLE}/kinematics_photon2_${SAMPLE}.png>
+<img height="600" src="http://torimoto.web.cern.ch/torimoto/physics/diphoton/${DATA}/${SAMPLE}/kinematics_photon2_${SAMPLE}.png">
 </A>
 
 <h2> Photon2 HCAL & ECAL Isolation </h2>
-<A HREF=http://torimoto.web.cern.ch/torimoto/physics/diphoton/mc/${SAMPLE}/caloIso_photon2_${SAMPLE}.png>
-<img height="600" src="http://torimoto.web.cern.ch/torimoto/physics/diphoton/mc/${SAMPLE}/caloIso_photon2_${SAMPLE}.png">
+<A HREF=http://torimoto.web.cern.ch/torimoto/physics/diphoton/${DATA}/${SAMPLE}/caloIso_photon2_${SAMPLE}.png>
+<img height="600" src="http://torimoto.web.cern.ch/torimoto/physics/diphoton/${DATA}/${SAMPLE}/caloIso_photon2_${SAMPLE}.png">
 </A>
 
 <h2> Photon2 Track Isolation</h2>
-<A HREF=http://torimoto.web.cern.ch/torimoto/physics/diphoton/mc/${SAMPLE}/trackIso_photon2_${SAMPLE}.png>
-<img height="600" src="http://torimoto.web.cern.ch/torimoto/physics/diphoton/mc/${SAMPLE}/trackIso_photon2_${SAMPLE}.png">
+<A HREF=http://torimoto.web.cern.ch/torimoto/physics/diphoton/${DATA}/${SAMPLE}/trackIso_photon2_${SAMPLE}.png>
+<img height="600" src="http://torimoto.web.cern.ch/torimoto/physics/diphoton/${DATA}/${SAMPLE}/trackIso_photon2_${SAMPLE}.png">
 </A>
 
 <h2> Photon2 Spike Rejection</h2>
-<A HREF=http://torimoto.web.cern.ch/torimoto/physics/diphoton/mc/${SAMPLE}/spike_photon2_${SAMPLE}.png>
-<img height="600" src="http://torimoto.web.cern.ch/torimoto/physics/diphoton/mc/${SAMPLE}/spike_photon2_${SAMPLE}.png">
+<A HREF=http://torimoto.web.cern.ch/torimoto/physics/diphoton/${DATA}/${SAMPLE}/spike_photon2_${SAMPLE}.png>
+<img height="600" src="http://torimoto.web.cern.ch/torimoto/physics/diphoton/${DATA}/${SAMPLE}/spike_photon2_${SAMPLE}.png">
 </A>
 
 <h2> DiPhoton plots</h2>
-<A HREF=http://torimoto.web.cern.ch/torimoto/physics/diphoton/mc/${SAMPLE}/diphoton_${SAMPLE}.png>
-<img height="600" src="http://torimoto.web.cern.ch/torimoto/physics/diphoton/mc/${SAMPLE}/diphoton_${SAMPLE}.png">
+<A HREF=http://torimoto.web.cern.ch/torimoto/physics/diphoton/${DATA}/${SAMPLE}/diphoton_${SAMPLE}.png>
+<img height="600" src="http://torimoto.web.cern.ch/torimoto/physics/diphoton/${DATA}/${SAMPLE}/diphoton_${SAMPLE}.png">
 </A>
 
 </FONT>
