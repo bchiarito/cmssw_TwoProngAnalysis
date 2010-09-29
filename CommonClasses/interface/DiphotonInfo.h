66be7aa09bea603fa5368579d766d6792896e579
chenders <>
2010-09-29 16:58:21 +0000

    now can do diphoton info for gen and reco photons

--- CommonClasses/interface/DiphotonInfo.h
+++ CommonClasses/interface/DiphotonInfo.h
@@ -33,23 +33,44 @@ namespace ExoDiPhotons{
   };
 
   std::string diphotonInfoBranchDefString("Minv/D:qt:deltaPhi:deltaEta:deltaR:cosThetaStar");
+  
 
-  void FillDiphotonInfo(diphotonInfo_t &fDiphotonInfo, const reco::Photon *photon1, const reco::Photon *photon2) {
-
-    reco::LeafCandidate::LorentzVector photon_vector1 = photon1->p4();
-    reco::LeafCandidate::LorentzVector photon_vector2 = photon2->p4();
+    // the internal function which gets called by the others
+  void FillDiphotonInfo(diphotonInfo_t &fDiphotonInfo, reco::LeafCandidate::LorentzVector photon_vector1, reco::LeafCandidate::LorentzVector photon_vector2) {
 
     fDiphotonInfo.Minv = (photon_vector1+photon_vector2).M();
     // pt of the pair
     fDiphotonInfo.qt = (photon_vector1+photon_vector2).pt();
     // there's a CMS function for deltaPhi in DataFormats/Math
-    fDiphotonInfo.deltaPhi = reco::deltaPhi(photon1->phi(),photon2->phi());
-    fDiphotonInfo.deltaEta = photon1->eta()-photon2->eta(); // always highest pt - second
+    fDiphotonInfo.deltaPhi = reco::deltaPhi(photon_vector1.phi(),photon_vector2.phi());
+    fDiphotonInfo.deltaEta = photon_vector1.eta()-photon_vector2.eta(); // always highest pt - second
     fDiphotonInfo.deltaR = TMath::Sqrt(fDiphotonInfo.deltaPhi*fDiphotonInfo.deltaPhi+fDiphotonInfo.deltaEta*fDiphotonInfo.deltaEta);
     fDiphotonInfo.cosThetaStar = -999.99; // need to calculate this!
-    
+
+
+    }
+
+  // filling function for reco photons  
+  void FillDiphotonInfo(diphotonInfo_t &fDiphotonInfo, const reco::Photon *photon1, const reco::Photon *photon2) {
+
+    reco::LeafCandidate::LorentzVector photon_vector1 = photon1->p4();
+    reco::LeafCandidate::LorentzVector photon_vector2 = photon2->p4();
+
+    FillDiphotonInfo(fDiphotonInfo,photon_vector1,photon_vector2);
   }
 
+  // same function, but for MC signal photons
+    void FillDiphotonInfo(diphotonInfo_t &fDiphotonInfo, const reco::GenParticle *photon1, const reco::GenParticle *photon2) {
+      
+      reco::LeafCandidate::LorentzVector photon_vector1 = photon1->p4();
+      reco::LeafCandidate::LorentzVector photon_vector2 = photon2->p4();
+      
+      FillDiphotonInfo(fDiphotonInfo,photon_vector1,photon_vector2);
+      
+    }
+
+
+  
 
 
 } //end of namespace
