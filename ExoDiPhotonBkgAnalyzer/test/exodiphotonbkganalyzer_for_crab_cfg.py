8482bde6dc3fdf029cd6718acf0c32d791da18a9
torimoto <>
2010-10-05 09:47:23 +0000

    removed "Begin processing ... " messages

--- ExoDiPhotonBkgAnalyzer/test/exodiphotonbkganalyzer_for_crab_cfg.py
+++ ExoDiPhotonBkgAnalyzer/test/exodiphotonbkganalyzer_for_crab_cfg.py
@@ -3,6 +3,10 @@ import FWCore.ParameterSet.Config as cms
 process = cms.Process("DiPhotonBkgAnalysis")
 
 process.load("FWCore.MessageService.MessageLogger_cfi")
+process.MessageLogger = cms.Service("MessageLogger",
+    cout = cms.untracked.PSet( threshold = cms.untracked.string('ERROR') ),
+    destinations = cms.untracked.vstring('cout')
+)
 
 process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(-1) )
 
