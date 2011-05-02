17179c5823be0cbf3e21daae057181f31624a939
yma <>
2011-05-02 17:05:57 +0000

    configuration to run on 311X MC (spring 11)

---
+++ ExoDiPhotonAnalyzer/python/exodiphotonanalyzer_spring11MC_cfi.py
@@ -0,0 +1,10 @@
+import FWCore.ParameterSet.Config as cms
+
+diphotonAnalyzer = cms.EDAnalyzer('ExoDiPhotonAnalyzer',
+                                  photonCollection = cms.untracked.InputTag("photons"),
+                                  ptMin = cms.untracked.double(10),
+                                  hltResults = cms.untracked.InputTag("TriggerResults","","REDIGI311X"),
+                                  L1Results = cms.untracked.InputTag("gtDigis"),
+                                  removeSpikes = cms.untracked.bool(False),
+                                  requireTightPhotons = cms.untracked.bool(True)
+)
