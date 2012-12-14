c7fea79b6dc01aa6740163ab6a3e8c8b2794dfa4
charaf <>
2012-12-14 16:32:42 +0000

    Added parameters to choose between Detector and Particle Flow selection

--- ExoDiPhotonAnalyzer/exodiphotonanalyzer_cfg.py
+++ ExoDiPhotonAnalyzer/exodiphotonanalyzer_cfg.py
@@ -5,30 +5,11 @@ process = cms.Process("ExoDiPhotonAnalysis")
 process.load("FWCore.MessageService.MessageLogger_cfi")
 
 process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(-1) )
-# just for testing!
-#process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(10000) )
 
 process.source = cms.Source("PoolSource",
     # replace 'myfile.root' with the source file you want to use
     fileNames = cms.untracked.vstring(
-        'file:diphoton_tree_testsample_Born250toInf.root',
-#    'file:/tmp/chenders/02071949-FA40-DF11-9990-001A64789DEC.root',
-#'file:/afs/cern.ch/cms/CAF/CMSCOMM/COMM_ECAL/torimoto/0065C919-F53B-DF11-8BF5-001D09F29146.root'
-        #direct from castor
-#        '/store/data/Commissioning10/MinimumBias/RAW-RECO/v8/000/132/601/02071949-FA40-DF11-9990-001A64789DEC.root'
-#    '/store/data/Run2010A/EG/RECO/v4/000/142/040/FE902386-B79C-DF11-88BF-00304879FC6C.root'
-    # a 383 prompt reco file on castor
-#    '/store/data//Run2010B//Photon/RECO/PromptReco-v2/000/146/944/00056B4C-7CCC-DF11-BAF2-00304879FA4C.root'
-#    '/store/data//Run2010B/Photon/RECO/PromptReco-v2/000/148/819/02B8C9B5-45E0-DF11-8310-001D09F295A1.root',
-#    '/store/data//Run2010B/Photon/RECO/PromptReco-v2/000/148/819/0636566B-59E0-DF11-A0C2-0019B9F70607.root'
-#    'rfio:/castor/cern.ch/user/c/chenders/DiPhotonBorn_Pt25to250_Spring10_GEN-SIM-RECO_200events.root',
-#    'rfio:/castor/cern.ch/user/y/yma/RSGravitons/Nov2010/RShighest.root',
-#    '/store/data/Run2010B/Photon/RECO/PromptReco-v2/000/148/953/642845D7-C0E1-DF11-B968-0030487A18F2.root'
-#  '/store/data/Run2012A/Photon/AOD/PromptReco-v1/000/190/684/F652691B-0383-E111-9821-001D09F241F0.root'
-#    'rfio:/castor/cern.ch/cms/store/data/Run2012A/Photon/AOD/PromptReco-v1/000/190/738/E4AD6153-0684-E111-AAE3-001D09F24DA8.root'
-#     'rfio:/castor/cern.ch/user/j/jcarson/DiPhotonTrees/Data/HighMassSkims_April27_Mgreater500.root'      
-#      'HighMassSkims_April27_Mgreater500.root'
-#      'diphoton_tree_testsample_Born250toInf.root'
+'root://eoscms//eos/cms/store/data/Run2012C/DoublePhotonHighPt/AOD/PromptReco-v2/000/201/602/CAAA8533-D1EF-E111-87A1-0025901D5C80.root'
      )
 )
 
@@ -37,26 +18,19 @@ process.source = cms.Source("PoolSource",
 process.load('Configuration/StandardSequences/FrontierConditions_GlobalTag_cff')
 
 #use the right global tag!
-#global tag fro >382 data (Nov22)
-#Aug11th Tag
-process.GlobalTag.globaltag = 'START52_V11C::All'
-#process.GlobalTag.globaltag = 'GR_R_53_V8::All'
-# global tag for prompt reco with 38X (as of Sept30)
-#process.GlobalTag.globaltag = 'GR10_P_V9::All'
-# this is global tag for PromptReco with 36X
-#process.GlobalTag.globaltag = 'GR10_P_V7::All'
-# this is the tag claimed for data reprocessing with 35X
-#process.GlobalTag.globaltag = 'GR_R_35X_V8::All'
-# and this is the tag for prompt reco with 35X
-#process.GlobalTag.globaltag = 'GR10_P_V5::All'
+## #global tag for 2012 A+B rereco within 532patch4
+## process.GlobalTag.globaltag = 'FT_53_V6_AN1::All'
+#global tag for 2012C v1+v2 within 532patch4
+process.GlobalTag.globaltag = 'GR_P_V40_AN1::All'
 
 # geometry for ecal 
-#When in 5_3_X Need to use diff Geometry
-process.load("Configuration.StandardSequences.Geometry_cff")
+#When in 5_3_X Need to use diff GeometryDB
+##process.load("Configuration.StandardSequences.Geometry_cff")
+process.load("Configuration.StandardSequences.GeometryDB_cff")
 
 # file for all histograms for all modules
 process.TFileService = cms.Service("TFileService",
-    fileName = cms.string('diphoton_tree_DiPhotonBorn_Pt250ToInf_Summer12.root')
+    fileName = cms.string('ExoDiPhotonAnalyzer.root')
 )
 
 # filter on good vertex
@@ -96,6 +70,8 @@ process.diphotonAnalyzer.ptMin = 70 # pt cut on all photons
 process.diphotonAnalyzer.removeSpikes = False # ie spikes will be exlcuded from tree
 process.diphotonAnalyzer.requireTightPhotons = False # ie only tight photons will be written 
 process.diphotonAnalyzer.isMC = False #MC = True or  Data = False
+process.diphotonAnalyzer.IDMethod = cms.untracked.string("ParticleFlow")
+process.diphotonAnalyzer.PFIDCategory = cms.untracked.string("Medium")
 
 # If running on data the following four entries should not be changed. They are loaded into the analyzer as strings but in the case isMC = False then all the both old_pu_n and pu_n will both be filled with -9999.99
 
