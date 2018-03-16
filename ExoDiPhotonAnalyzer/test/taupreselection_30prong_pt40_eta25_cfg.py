9cf614ae4ab99379c7b6e3b3a3d304003f7b4c77
Brandon Chiarito <bchiarito@gmail.com>
2018-03-16 19:05:21 -0400

    new _cfgs to run the tau preselection

---
+++ ExoDiPhotonAnalyzer/test/taupreselection_30prong_pt40_eta25_cfg.py
@@ -0,0 +1,28 @@
+import FWCore.ParameterSet.Config as cms
+
+process = cms.Process("TAUFILT")
+
+process.source = cms.Source ("PoolSource",
+  fileNames = cms.untracked.vstring (
+  "/store/mc/RunIISummer16MiniAODv2/DYJetsToLL_M-50_TuneCUETP8M1_13TeV-madgraphMLM-pythia8/MINIAODSIM/PUMoriond17_80X_mcRun2_asymptotic_2016_TrancheIV_v6_ext2-v1/100000/00099D43-77ED-E611-8889-5065F381E1A1.root"
+  ))
+
+process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32 (10000))
+process.load("FWCore.MessageService.MessageLogger_cfi")
+process.MessageLogger.cerr.FwkReport.reportEvery = 1000
+
+process.filt = cms.EDFilter('ZtoTauHad',
+  tree = cms.untracked.bool(False),
+  filterByTruthDecayType = cms.untracked.vdouble(5.4),
+  ptMin = cms.untracked.double(40.0),
+  absEtaMax = cms.untracked.double(2.5),
+  )
+
+process.out = cms.OutputModule("PoolOutputModule", 
+  fileName = cms.untracked.string("MINIAOD.root"),
+  SelectEvents = cms.untracked.PSet( SelectEvents = cms.vstring("p") )
+  )
+
+process.p = cms.Path(process.filt)
+
+process.end = cms.EndPath(process.out)
