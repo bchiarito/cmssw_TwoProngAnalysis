33251205f4298e49e547e7efc43d5580993132de
Brandon Chiarito <bchiarito@gmail.com>
2018-11-12 11:17:17 -0500

    add command line option to gendecaychain

--- ZTauTauFilters/test/genDecayChain_cfg.py
+++ ZTauTauFilters/test/genDecayChain_cfg.py
@@ -1,4 +1,8 @@
 import FWCore.ParameterSet.Config as cms
+from FWCore.ParameterSet.VarParsing import VarParsing
+options = VarParsing ("python")
+options.register("file", "", VarParsing.multiplicity.singleton, VarParsing.varType.string, "")
+options.parseArguments()
 
 process = cms.Process("GENANA")
 process.load("SimGeneral.HepPDTESSource.pythiapdt_cfi")
@@ -11,7 +15,9 @@ process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(3) )
 process.source = cms.Source("PoolSource",
         fileNames = cms.untracked.vstring(
         #'/store/mc/RunIISummer16MiniAODv2/DYJetsToLL_M-10to50_TuneCUETP8M1_13TeV-madgraphMLM-pythia8/MINIAODSIM/PUMoriond17_80X_mcRun2_asymptotic_2016_TrancheIV_v6-v1/120000/00271851-3CC8-E611-AFB9-002590D0AFD8.root' # 10-50
-        '/store/mc/RunIISummer16MiniAODv2/DYJetsToLL_M-50_TuneCUETP8M1_13TeV-madgraphMLM-pythia8/MINIAODSIM/PUMoriond17_80X_mcRun2_asymptotic_2016_TrancheIV_v6_ext2-v1/100000/00099D43-77ED-E611-8889-5065F381E1A1.root' # 50+
+        #'/store/mc/RunIISummer16MiniAODv2/DYJetsToLL_M-50_TuneCUETP8M1_13TeV-madgraphMLM-pythia8/MINIAODSIM/PUMoriond17_80X_mcRun2_asymptotic_2016_TrancheIV_v6_ext2-v1/100000/00099D43-77ED-E611-8889-5065F381E1A1.root' # 50+
+        #'file:pickevents2.root'
+        options.file
 )
 )
 
